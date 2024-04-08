2a8cbb07f46cb34829d8ef96bdc56b9e427aeec8
forgetme-not <gijae8888@naver.com>
2024-04-08 17:03:27 +0900

    pgj 회원가입추가

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
 import App from "./App.jsx";
+import Join from "./pages/join/join.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App></App>
+        <Join></Join>
     </React.StrictMode>
 )
