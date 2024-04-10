22f28ef82beb53b20526ab4339f196dd0dae51df
seyun00 <reny83@naver.com>
2024-04-10 15:42:27 +0900

    create header component_csy

---
+++ client/src/components/header/header.jsx
@@ -0,0 +1,23 @@
+import './header.css'
+
+export default function header() {
+  
+  return (
+    <header>
+      <div className='header'>
+        <div className='header_logo_menu'>
+          <a href="#" className="header_logo">JoyLink</a>
+          <ul className='header_menu'>
+            <ol><a href="#" className="header_club">모임찾기</a></ol>
+            <ol><a href="#" className="header_notice">공지사항</a></ol>
+            <ol><a href="#" className="header_VR">VR체험관</a></ol>
+          </ul>
+        </div>
+        <div className='header_button'>
+          <button className="header_login_button">로그인</button>
+          <button className="header_signup_button">회원가입</button>
+        </div>
+      </div>
+    </header>
+  )
+}
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
-import FindIdPw from "./pages/findIdPw/findIdPw.jsx";
-
+// import FindIdPw from "./pages/findIdPw/findIdPw.jsx";
+import Header from "./components/header/header";
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FindIdPw></FindIdPw>
+        <Header />
     </React.StrictMode>
 )
