ed99219e66848d21ef685c82a7f77dfae33a52a5
Gaeol <ahffkd29@gmail.com>
2024-04-10 15:28:44 +0900

    gaeol_clubintro 초안완성

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
-import FindIdPw from "./pages/findIdPw/findIdPw.jsx";
+import ClubIntro from "./pages/clubPromotion/clubIntro";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FindIdPw></FindIdPw>
+        <ClubIntro/>
     </React.StrictMode>
 )
