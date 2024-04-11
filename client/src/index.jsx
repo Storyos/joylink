8093a0b04ca1ca5088973ff8424e0b4b10a76867
seyun00 <reny83@naver.com>
2024-04-11 10:23:30 +0900

    create mypage_csy

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
-import Header from "./components/header/header";
-import Footer from "./components/footer/footer";
+import Mypage from "./pages/mypage/mypage";
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Header />
-        <Footer />
+        <Mypage />
     </React.StrictMode>
 )
