9d38866636a1bb6d49b6befb728120236d4e36b4
Gaeol <ahffkd29@gmail.com>
2024-04-12 09:52:52 +0900

    gaeol git test

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
-import ClubJoin from "./pages/clubPromotion/clubJoin";
+import Header from "./components/header/header"
+import Footer from "./components/footer/footer"
+import './index.css' // tailwindcss 적용을 위한 css 파일
 
-ReactDOM.ClubJoin(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <ClubJoin/>
+        <Header />
+        <h1 className="m-20 text-3xl font-bold underline">테일윈드 테스트 - src/index.jsx</h1>
+        <Footer />
     </React.StrictMode>
 )
-
