b91fd7df4bacdb816a6ed925ab6d2faf604737cf
Gaeol <ahffkd29@gmail.com>
2024-04-12 09:05:01 +0900

    gaeol git test

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
-import ClubIntro from "./pages/clubPromotion/clubIntro";
+import ClubJoin from "./pages/clubPromotion/clubJoin";
 
-ReactDOM.ClubCreate(document.getElementById('root')).render(
+ReactDOM.ClubJoin(document.getElementById('root')).render(
     <React.StrictMode>
-        <FindIdPw></FindIdPw>
+        <ClubJoin/>
     </React.StrictMode>
 )
 
