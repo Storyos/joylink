6d0ca1e7702c1640ff528ac7d0d99a7197604c9a
seyun00 <reny83@naver.com>
2024-04-12 10:57:16 +0900

    message modal box 작업중_csy

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
-import Header from "./components/header/header"
-import Footer from "./components/footer/footer"
+import Mypage from "./pages/mypage/mypage";
+
 import './index.css' // tailwindcss 적용을 위한 css 파일
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Header />
-        <h1 className="m-20 text-3xl font-bold underline">테일윈드 테스트 - src/index.jsx</h1>
-        <Footer />
+        <Mypage />
     </React.StrictMode>
 )
 
--- client/src/pages/mypage/mypage.jsx
+++ client/src/pages/mypage/mypage.jsx
@@ -8,6 +8,8 @@ export default function Mypage () {
   return (
     <>
       <Header />
+      
+      {/* 메인 영역 */}
       <div className='mypage_box'>
         <h2 className='mypage_title'>MyPage</h2>
         <div className='mypage_content_box'>
@@ -30,7 +32,54 @@ export default function Mypage () {
           </div>
         </div>
       </div>
+
+      {/* 쪽지 modal box */}
+      <div className='fixed inset-0 p-4 m-auto bg-white border border-black border-solid' style={{width:'650px',height:'650px'}} >
+        <h1 className='mb-4' >쪽지</h1>
+        
+        {/* 상단 버튼 */}
+        <div className='flex justify-between mb-4'>
+          <span className='p-1 border-2'>
+            <button className='mx-4'>받은 쪽지</button>
+            <button className='mx-4'>보낸 쪽지</button>
+          </span>
+          <button className='p-1 border-2'>쪽지 쓰기</button>
+        </div>
+          
+        {/* 리스트 상단 */}
+        <div className='flex p-2 mb-4 border-2'>
+          <input type="checkbox" />
+          <div className='flex justify-center w-full mx-2'>
+            <button>보낸 사람</button>
+            <button className='mx-48'>제목</button>
+            <button>날짜</button>
+          </div>
+        </div>
+        
+        {/* 쪽지 리스트 */}
+        <div className='border-2'>
+          <div className='flex p-2 mb-4'>
+            <input type="checkbox" />
+            <div className='flex justify-between w-full mx-2'>
+              <div>
+                <button className='ml-1 text-center w-28'>user</button>
+                <button className='ml-2'>동아리 오리엔테이션 일정 공지</button>
+              </div>
+              <button className=''>2024-04-12</button>
+            </div>
+          </div>
+        </div>
+
+        {/* 하단 버튼 */}
+        <div className='flex justify-end'>
+          <button className='p-1 px-3 border-2'>삭제</button>
+          <button className='p-1 px-3 border-2'>닫기</button>
+        </div>
+        
+      </div>
+      
       <Footer />
+
     </>
   )
 }
