d962294f21a4bdfd7c5d802c66e8d0881db487d8
seyun00 <reny83@naver.com>
2024-04-17 09:44:52 +0900

    update notice_csy

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -3,16 +3,19 @@ import Mypage from './pages/mypage/mypage';
 import Login from './pages/login/login';
 import Join from './pages/join/join';
 import App from "./App";
+import NoticePageWithSidebar from "./pages/notice/noticepage";
 
 export default function Main() {
   return (
       <BrowserRouter>
         <Routes>
             <Route path='/' element={<App/>} >
+              <Route index element={<Mypage/>}/>
               <Route path='mypage' element={<Mypage/>}/>
-              <Route path='login' element={<Login/>}/>
-              <Route path='join' element={<Join/>}/>
+              <Route path="notice" element={<NoticePageWithSidebar/>}/>
             </Route>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/join' element={<Join/>}/>
         </Routes>
       </BrowserRouter>
   );
