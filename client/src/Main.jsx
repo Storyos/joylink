b9e9926765de544ce74e04844aa69962c3e43114
seyun00 <reny83@naver.com>
2024-04-19 11:48:59 +0900

    notice페이지 작업 완료_csy

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -1,21 +1,26 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+import Mainpage from "./pages/mainpage/mainpage";
 import Mypage from './pages/mypage/mypage';
 import Login from './pages/login/login';
 import Join from './pages/join/join';
-import App from "./App";
 import Notice from "./pages/notice/notice";
+import CbSearch from "./pages/clubPromotion/cbsearch";
 
 export default function Main() {
   return (
       <BrowserRouter>
         <Routes>
-            <Route path='/' element={<App/>} >
-              <Route index element={<Mypage/>}/>
-              <Route path='mypage' element={<Mypage/>}/>
+            <Route path="/" element={<App/>} >
+              <Route index element={<Mainpage/>}/>
+              <Route path="mypage" element={<Mypage/>}/>
               <Route path="notice" element={<Notice/>}/>
+              <Route path="notice/${notice.id}" element={<Notice/>}/>
+              <Route path="notice/${event.id}" element={<Notice/>}/>
+              <Route path="clubpromotionweb" element={<CbSearch/>}/>
             </Route>
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/join' element={<Join/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/join" element={<Join/>}/>
         </Routes>
       </BrowserRouter>
   );
