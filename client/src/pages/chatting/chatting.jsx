6b355d47a68163938480c98dc3e301c5d9fc2a0f
Storyos <storyoser@pukyong.ac.kr>
2024-04-23 10:19:00 +0900

    chatting 일시구현 완료

--- client/src/pages/chatting/chatting.jsx
+++ client/src/pages/chatting/chatting.jsx
@@ -48,18 +48,24 @@ export default function Chatting() {
     }, [myemail]); // myemail이 변경될 때마다 이 효과 실행
 
 
-
     // clubseq는 clubpage가 완성되면 거기서 받아올 예정.
     // 현재는 clubseq는 상수값을 넣어서 test할 예정. (04/22) 추후 수정 필요
+
     const handleInserts = (payload) => {
-        // 여기서 insert된 메시지 출력 예정
-        console.log('Message Received!', payload)
-        async function getMessages() {
-            const { data, error } = await supabase.from('chats').select('chat_content').eq('club_seq', 1);
-            setMessages(data);
-        }
-        getMessages();
+        console.log('Message Received!', payload);
+        const newMessage = payload.new; // 'new' 프로퍼티에 새 메시지 정보가 들어있다고 가정합니다.
+        setMessages([...messages, newMessage]);
     }
+    
+    // const handleInserts = (payload) => {
+    //     // 여기서 insert된 메시지 출력 예정
+    //     console.log('Message Received!', payload.new)
+
+    //     async function getMessages() {
+    //         setMessages([...messages,payload.new]);
+    //     }
+    //     getMessages();
+    // }
 
     // 메시지 전송
     // 추후 club_seq 정보 수정 필요!!!
@@ -75,10 +81,9 @@ export default function Chatting() {
         catch (error) {
             console.error("메시지 전송 에러", error);
         }
-
     }
-    // 
 
+    // 
     // 채팅 전송
     const sendMessage = (e) => {
         e.preventDefault();
@@ -95,7 +100,6 @@ export default function Chatting() {
                     <div key={index}>{msg.chat_content}</div> // msg.chat_content로 가정합니다.
                 ))}
             </div>
-
             <form onSubmit={sendMessage}>
                 <input
                     type="text"
