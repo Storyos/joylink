4c3b4bdb8dfb776ce0853155fd8ddcfa9f39345c
seyun00 <reny83@naver.com>
2024-04-24 09:19:18 +0900

    오류 수정_csy

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
-import Header from "./components/header/header"
-import Footer from "./components/footer/footer"
+import Main from "./Main";
+
 import './index.css' // tailwindcss 적용을 위한 css 파일
-import CbSearch from "./pages/clubPromotion/cbsearch";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <Header />
-        <CbSearch/>
-        <Footer />
+      <Main />
     </React.StrictMode>
 )
