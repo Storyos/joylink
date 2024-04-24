a2c294e46d1ab3bda93804577bba645c6c0aa533
Gaeol <ahffkd29@gmail.com>
2024-04-24 09:13:22 +0900

    확인

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client'
 import Header from "./components/header/header"
 import Footer from "./components/footer/footer"
 import './index.css' // tailwindcss 적용을 위한 css 파일
-import CbSearch from "./pages/clubPromotion/cbsearch";
+import CbCreate from "./pages/cbCreate/cbCreate";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Header />
-        <CbSearch/>
+        <CbCreate/>
         <Footer />
     </React.StrictMode>
 )
