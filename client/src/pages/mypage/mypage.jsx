80e8f3f3f89c19c5f2c99e0858e68aa021d31447
seyun00 <reny83@naver.com>
2024-05-08 08:40:54 +0900

    mypage 디자인 수정_csy

--- client/src/pages/mypage/mypage.jsx
+++ client/src/pages/mypage/mypage.jsx
@@ -24,22 +24,22 @@ export default function Mypage () {
   }
 
   return (
-    <>
+    <div className='bg-gray-100'>
 
       {/* 메인 영역 */}
-      <div className='pt-12 pb-24 mx-80 rounded-3xl'>
+      <div className='pt-12 pb-24 mx-80'>
         <p className='p-1 mb-4 text-2xl font-bold'>마이페이지</p>
         <div className='flex justify-between'>
-          <div className='w-[12%] rounded-[10px] bg-[#e9e9e9]'>
+          <div className='w-[12%] rounded-[10px] bg-white shadow '>
             <div className='flex flex-col items-center min-h-[500px] py-4'>
-              <button className={'font-bold text-[#a9a9a9] my-2 '+ (mypageMenu === 'userInfo' && 'text-black')} onClick={() => handleMypageMenu("userInfo")}>내 정보</button>
-              <button className={'font-bold text-[#a9a9a9] my-2 '+ (mypageMenu === 'updateInfo' && 'text-black')} onClick={() => handleMypageMenu("updateInfo")}>정보 수정</button>
-              <button className={'font-bold text-[#a9a9a9] my-2 '+ (mypageMenu === 'myclub' && 'text-black')} onClick={() => handleMypageMenu("myclub")}>내 동아리</button>
-              <button className='font-bold text-[#a9a9a9] my-2' onClick={handleOpenMessage}>쪽지</button>
-              <button className={'font-bold text-[#a9a9a9] my-2 '+ (mypageMenu === 'application' && 'text-black')} onClick={() => handleMypageMenu("application")}>신청 현황</button>
+              <button className={'font-bold text-[#a9a9a9] my-2 text-sm '+ (mypageMenu === 'userInfo' && 'text-black text-lg')} onClick={() => handleMypageMenu("userInfo")}>내 정보</button>
+              <button className={'font-bold text-[#a9a9a9] my-2 text-sm '+ (mypageMenu === 'updateInfo' && 'text-black')} onClick={() => handleMypageMenu("updateInfo")}>정보 수정</button>
+              <button className={'font-bold text-[#a9a9a9] my-2 text-sm '+ (mypageMenu === 'myclub' && 'text-black')} onClick={() => handleMypageMenu("myclub")}>내 동아리</button>
+              <button className='font-bold text-[#a9a9a9] my-2 text-sm ' onClick={handleOpenMessage}>쪽지</button>
+              <button className={'font-bold text-[#a9a9a9] my-2 text-sm '+ (mypageMenu === 'application' && 'text-black')} onClick={() => handleMypageMenu("application")}>신청 현황</button>
             </div>
           </div>
-          <div className='w-4/5 p-8 rounded-xl bg-[#e9e9e9] ml-10'>
+          <div className='w-4/5 p-8 ml-10 bg-white shadow rounded-xl'>
             
             {/* 내 정보 */}
             {mypageMenu == "userInfo" &&
@@ -78,13 +78,13 @@ export default function Mypage () {
               <div className='w-[70%]'>
                 <p className='mb-8 font-bold text-center'>정보 수정</p>
                 <ul>
-                  <li><label htmlFor="" className='text-sm'>아이디(이메일) <input type="text" className='w-full text-base rounded'/></label></li>
-                  <li><label htmlFor="" className='text-sm'>비밀번호 <input type="text" className='w-full text-base rounded'/></label></li>
-                  <li><label htmlFor="" className='text-sm'>이름 <input type="text" className='w-full text-base rounded'/></label></li>
-                  <li><label htmlFor="" className='text-sm'>전화번호 <input type="text" className='w-full text-base rounded'/></label></li>
+                  <li><label htmlFor="" className='text-sm text-[#a9a9a9]'>아이디(이메일) <input type="text" className='w-full p-1 px-2 text-black bg-gray-100 rounded' defaultValue={"아이디"}/></label></li>
+                  <li><label htmlFor="" className='text-sm text-[#a9a9a9]'>비밀번호 <input type="text" className='w-full p-1 px-2 text-black bg-gray-100 rounded' defaultValue={"비밀번호"}/></label></li>
+                  <li><label htmlFor="" className='text-sm text-[#a9a9a9]'>이름 <input type="text" className='w-full p-1 px-2 text-black bg-gray-100 rounded' defaultValue={"이름"}/></label></li>
+                  <li><label htmlFor="" className='text-sm text-[#a9a9a9]'>전화번호 <input type="text" className='w-full p-1 px-2 text-black bg-gray-100 rounded' defaultValue={"전화번호"}/></label></li>
                 </ul>
                 <div className='flex justify-end'>
-                  <button className='border-2 bg-[#c9c9c9] p-1 px-2 mt-2 rounded-[5px]'>수정</button>
+                  <button className='bg-gray-100 p-1 px-2 mt-2 rounded-[5px] hover:bg-[#e9e9e9]'>수정</button>
                 </div>
               </div>
             </div>
@@ -97,19 +97,19 @@ export default function Mypage () {
                 <caption className='mb-8 font-bold'>내가 가입한 동아리</caption>
                 <tr className='border-2 border-[#c9c9c9]'>
                   <th className='border-2 border-[#c9c9c9] w-[50px] p-2'> </th>
-                  <td className='border-2 border-[#c9c9c9] text-center min-w-[300px] p-2'>동아리 명</td>
+                  <td className='border-2 border-[#c9c9c9] text-center min-w-[300px] p-2 text-sm'>동아리 명</td>
                 </tr>
                 <tr className='border-2 border-[#c9c9c9]'>
                   <th className='border-2 border-[#c9c9c9] w-[50px] p-2'>1</th>
-                  <td className='border-2 border-[#c9c9c9] text-center min-w-[300px]'>동아리1</td>
+                  <td className='border-2 border-[#c9c9c9] text-center min-w-[300px] text-sm'>동아리1</td>
                 </tr>
                 <tr className='border-2 border-[#c9c9c9]'>
                   <th className='border-2 border-[#c9c9c9] w-[50px] p-2'>2</th>
-                  <td className='border-2 border-[#c9c9c9] text-center min-w-[300px]'>동아리2</td>
+                  <td className='border-2 border-[#c9c9c9] text-center min-w-[300px] text-sm'>동아리2</td>
                 </tr>
                 <tr className='border-2 border-[#c9c9c9]'>
                   <th className='border-2 border-[#c9c9c9] w-[50px] p-2'>3</th>
-                  <td className='border-2 border-[#c9c9c9] text-center min-w-[300px]'>동아리3</td>
+                  <td className='border-2 border-[#c9c9c9] text-center min-w-[300px] text-sm'>동아리3</td>
                 </tr>
               </table>
             </div>
@@ -122,23 +122,23 @@ export default function Mypage () {
                 <caption className='mb-8 font-bold'>신청 중인 동아리</caption>
                 <tr className='border-2 border-[#c9c9c9]'>
                   <th className='border-2 border-[#c9c9c9] w-[50px]'> </th>
-                  <td className='border-2 border-[#c9c9c9] text-center min-w-[300px] p-2'>동아리 명</td>
-                  <td className='border-2 border-[#c9c9c9] text-center'>신청 현황</td>
+                  <td className='border-2 border-[#c9c9c9] text-center min-w-[300px] p-2 text-sm'>동아리 명</td>
+                  <td className='border-2 border-[#c9c9c9] text-center text-sm'>신청 현황</td>
                 </tr>
                 <tr className='border-2 border-[#c9c9c9]'>
                   <th className='border-2 border-[#c9c9c9] w-[50px]'>1</th>
-                  <td className='border-2 border-[#c9c9c9] text-center min-w-[300px] p-2'>동아리1</td>
-                  <td className='border-2 border-[#c9c9c9] text-center'>신청 중</td>
+                  <td className='border-2 border-[#c9c9c9] text-center min-w-[300px] p-2 text-sm'>동아리1</td>
+                  <td className='border-2 border-[#c9c9c9] text-center text-sm'>신청 중</td>
                 </tr>
                 <tr className='border-2 border-[#c9c9c9]'>
                   <th className='border-2 border-[#c9c9c9] w-[50px]'>2</th>
-                  <td className='border-2 border-[#c9c9c9] text-center min-w-[300px] p-2'>동아리2</td>
-                  <td className='border-2 border-[#c9c9c9] text-center'>가입 완료</td>
+                  <td className='border-2 border-[#c9c9c9] text-center min-w-[300px] p-2 text-sm'>동아리2</td>
+                  <td className='border-2 border-[#c9c9c9] text-center text-sm'>가입 완료</td>
                 </tr>
                 <tr className='border-2 border-[#c9c9c9]'>
                   <th className='border-2 border-[#c9c9c9] w-[50px]'>3</th>
-                  <td className='border-2 border-[#c9c9c9] text-center min-w-[300px] p-2'>동아리 명</td>
-                  <td className='border-2 border-[#c9c9c9] text-center'>신청 취소</td>
+                  <td className='border-2 border-[#c9c9c9] text-center min-w-[300px] p-2 text-sm'>동아리3</td>
+                  <td className='border-2 border-[#c9c9c9] text-center text-sm'>신청 취소</td>
                 </tr>
               </table>
             </div>
@@ -155,6 +155,6 @@ export default function Mypage () {
         <MessageModal handleCloseMessage={handleCloseMessage} handleOpenMessage={handleOpenMessage} />
       }
 
-    </>
+    </div>
   )
 }
