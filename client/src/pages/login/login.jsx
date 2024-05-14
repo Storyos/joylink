880887ec09f4dca916275e84eedf2e329a16d41b
Storyos <storyoser@pukyong.ac.kr>
2024-05-14 12:05:43 +0900

    집에가고싶은 커밋

--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -130,7 +130,6 @@ export default function Login() {
                     <h5 className="mt-2">계정이 없으신가요?</h5>
                     <div className="flex justify-center mt-4">
                         <button onClick={handleGoogleLogin} className="px-4 py-2 mr-2 font-bold text-white bg-red-500 rounded hover:bg-red-600">구글로그인</button>
-                        <button onClick={handleTest1} className="px-4 ,py-2 mr-2 font-bold text-white bg-gray-500 rounded hover:bg-gray-600">TEST Button</button>
                         <button id='join' onClick={handleTest1} className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-600">회원가입</button>
                     </div>
                 </div>
