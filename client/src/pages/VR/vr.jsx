40a7cd3cb1ab3ebdc9c34e71a1e9182d4271b887
Park HyeonJun <baghyeonjun16@gmail.com>
2024-05-21 11:43:43 +0900

    PHJ_vrtest

--- client/src/pages/VR/vr.jsx
+++ client/src/pages/VR/vr.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import "aframe";
 import "aframe-particle-system-component";
+import "aframe-extras"
+
 
 export default function Vr() {
-  const glbFilePath = "/doll_ringo_ani__physics_test.glb";
-  
+  const tent = "/tent.glb"
   return (
     <div>
       <h1>vr 페이지 입니다</h1>
       <a-scene className="aframe-scene">
-        <a-entity gltf-model={`url(${glbFilePath})`} position="0 0 -5"></a-entity>
+        <a-entity gltf-model={`url(${tent})`} 
+        position="0 0 -5"></a-entity>
       </a-scene>
     </div>
   );
 }
-
-
