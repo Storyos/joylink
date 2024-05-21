64157711177cdcb4eafea68fbcf69f6dbc239655
forgetme-not <gijae8888@naver.com>
2024-05-21 10:54:09 +0900

    pgj 동아리 상세페이지 디자인수정

--- client/src/components/myInfo/myInfo.jsx
+++ client/src/components/myInfo/myInfo.jsx
@@ -3,37 +3,65 @@ import { Link } from "react-router-dom";
 import ChattingModal from "../chattingModal";
 
 export default function MyInfo() {
-  
   const handleOpenChatting = () => {
-    window.open("/chatting","chatting","width=500, height=500, top=300, left=450")
+    window.open(
+      "/chatting",
+      "chatting",
+      "width=500, height=500, top=300, left=450"
+    );
   };
   return (
-    <div>
-      <div className="p-2 m-3 bg-gray-100 border-2 rounded-lg">
-        <h2>내 정보</h2>
+    <div className="border rounded-xl p-5 m-12">
+      <div className="p-1 m-2">
+        <h2 className="text-lg">내 정보</h2>
         <br></br>
+        <div className="text-sm space-y-1">
+          <h3>회원등급 : 평민</h3>
+          <h3>게시글 수 : 10</h3>
+          <h3>댓글 수 : 10</h3>
+        </div>
         <br></br>
+      </div>
+
+      <div className="border-t"></div>
+      <div className="p-1 m-2">
         <Link to="/clubManagementPage">
-          <button className="m-2">Club Management</button>
+          <button className="m-1">Club Management</button>
         </Link>
       </div>
-      <div className="p-2 m-3 bg-gray-100 border-2 border-2 rounded-lg">
+
+      <div className="border-t"></div>
+
+      <div className="p-1 m-3">
         <button onClick={handleOpenChatting}>채팅</button>
       </div>
-      <div className="border-2 p-2 rounded-lg ... m-3 bg-gray-100">
+
+      <div className="border-t"></div>
+
+      <div className="p-1 m-3">
         <Link to="/ViewAccountPage">
           <button>장부</button>
         </Link>
       </div>
-      <div className="border-2 p-2 rounded-lg ... m-3 bg-gray-100">
+
+      <div className="border-t"></div>
+
+      <div className="p-1 m-3">
         <h3>카테고리</h3>
         <br></br>
-        <br></br>
-        <div className="flex flex-col items-center">
-          <Link to="/gallery"><button>갤러리</button></Link>
-          <Link to="/clubNotice"><button>동아리 공지사항</button></Link>
-          <Link to="/freeBoard"><button>자유게시판</button></Link>
-          <Link to="/questionBoard"><button>질문게시판</button></Link>
+        <div className="flex flex-col items-center space-y-2">
+          <Link to="/gallery">
+            <button>갤러리</button>
+          </Link>
+          <Link to="/clubNotice">
+            <button>동아리 공지사항</button>
+          </Link>
+          <Link to="/freeBoard">
+            <button>자유게시판</button>
+          </Link>
+          <Link to="/questionBoard">
+            <button>질문게시판</button>
+          </Link>
         </div>
       </div>
     </div>
--- client/src/pages/myClubPage/myClub.jsx
+++ client/src/pages/myClubPage/myClub.jsx
@@ -22,12 +22,12 @@ export default function MyClubPage() {
         <MyInfo/> {/* 사이드바 */}
       </div>
       <div className="w-1/2 p-6">
-        <div className="flex items-center justify-between mb-4">
+        <div className="flex items-center justify-between m-6">
           <h1 className="text-xl font-bold">게시글</h1>
-          <input type="text" id="search" placeholder="게시글 검색" className="px-2 py-1 border-2 border-solid rounded " />
+          <input type="text" id="search" placeholder="게시글 검색" className="px-2 py-1 border rounded " />
         </div>
         <hr /><br />
-        <h2 className="mb-4 text-lg font-semibold bg-gray-100">게시글 목록</h2>
+        <h2 className="text-lg font-semibold my-6">게시글 목록</h2>
         {/* 여기에 게시글 목록 컴포넌트를 추가 */}
         <PostList />
       </div>
