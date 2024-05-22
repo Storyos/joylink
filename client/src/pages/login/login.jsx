1b1bc13c4f461211c8ec991ff973327c3e17b01c
Storyos <storyoser@pukyong.ac.kr>
2024-05-22 11:05:42 +0900

    로그인 페이지 수정

--- client/src/pages/login/login.jsx
+++ client/src/pages/login/login.jsx
@@ -36,14 +36,14 @@ export default function Login() {
             updateUserData(email);
         }
     }, [email]);
-    
+
 
     // 구글 로그인 처리
     const handleGoogleLogin = async () => {
         const { data, error } = await supabase.auth.signInWithOAuth({
             provider: "google",
-            options:{
-                redirectTo:'http://localhost:3000/checkUserInfo'
+            options: {
+                redirectTo: 'http://localhost:3000/checkUserInfo'
             }
         });
         if (error) console.log("구글 OAUTH에서 발생한에러 :", error);
@@ -63,7 +63,7 @@ export default function Login() {
             console.error('Error fetching data:', error);
         }
     }
-    
+
     // 이메일 인증여부 체크
     const handleverifyemail = async () => {
         const { data, error } = await supabase
@@ -72,33 +72,33 @@ export default function Login() {
             // .eq('user_email_verified', true)
             .eq('user_id', useremail)
             .single()
-        
-            console.log(data);
+
+        console.log(data);
         if (!data) {
             alert("이메일 인증이 아직 안되었습니다.");
-        }else{
+        } else {
             await handlelogin();
         }
     }
-    
+
     const fetchUserData = async () => {
         const { data, error } = await supabase
             .from('users')
             .select('*')
             .eq('user_id', useremail)
             .single()
-            
-        if(error){
-            console.error("fetch user에서 발생한 에러 ",error);
 
-        }else{
+        if (error) {
+            console.error("fetch user에서 발생한 에러 ", error);
+
+        } else {
             useUserStore.getState().setUser(data); // zustand에 로그인후 컬럼정보 저장
             alert("로그인 성공~");
             const userState = useUserStore.getState(); // 상태를 가져옵니다
-            console.log("상태저장 값:",userState.user); // 테스트용으로 
+            console.log("상태저장 값:", userState.user); // 테스트용으로 
             navigate('/');
         }
-        
+
     }
     // 로그인 요청 처리
     const handlelogin = async () => {
@@ -115,32 +115,65 @@ export default function Login() {
         }
     }
     return (
-        <div className="flex items-center justify-center h-screen">
-            <div className='boxGroup'>
-                <div className="p-4 bg-gray-100 rounded-lg loginBox">
-                    <div className="mb-4 inputBox">
-                        <input type="text" className="w-full px-4 py-2 border border-gray-300 rounded inputField focus:outline-none focus:border-blue-500" placeholder="아이디" value={email ? email : email} onChange={(e) => setEmail(e.target.value)} />
-                        <input type="password" className="w-full px-4 py-2 mt-2 border border-gray-300 rounded inputField focus:outline-none focus:border-blue-500" placeholder="비밀번호" onChange={(e) => setPassword(e.target.value)} />
-                    </div>
-                    <button className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded loginButton hover:bg-blue-600" onClick={handleverifyemail}>로그인</button>
-                </div>
-                <div className="mt-4 text-center findBox">
-                    <button id='findId' className="text-blue-500 hover:underline">아이디/비밀번호 찾기</button>
-                    <h5 className="mt-2">계정이 없으신가요?</h5>
-                    <div className="flex justify-center mt-4">
-                        <button onClick={handleGoogleLogin} className="px-4 py-2 mr-2 font-bold text-white bg-red-500 rounded hover:bg-red-600">구글로그인</button>
-                        <button id='join' onClick={()=>{navigate('/join')}} className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-600">회원가입</button>
-                    </div>
-                </div>
-            </div >
-            <div>
-                {(
-                    <div>
-                        <h3>현재 로그인된 Email: {email ? email : "Guest"}</h3>
-                    </div>
-                )}
+        <div className="flex h-screen">
+            <div className="w-1/2 h-full">
+                <img src="/assets/main7.jpg" alt="Image" className="object-cover w-full h-full" />
             </div>
-        </div >
+            <div className="flex items-center justify-center w-1/2 bg-white">
+                <div className="w-full max-w-md p-6">
+                    <div className="mb-8 text-center">
+                        <img src="/icons/joylink_blue_logo.png" alt="Logo" className="w-20 mx-auto mb-4" />
+                        <h2 className="text-2xl font-bold text-gray-700">Login</h2>
+                    </div>
+                    <div className="mb-4">
+                        <input
+                            type="text"
+                            className="w-full px-4 py-2 mb-3 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                            placeholder="E-mail Address" value={email ? email : email} onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <input
+                            type="password"
+                            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                            placeholder="Password"
+                            onChange={(e) => setPassword(e.target.value)} 
+                        />
+                        <div className="mt-2 text-right">
+                            <a href="#" className="text-sm text-blue-500 hover:underline">비밀번호를 잊어버리셨나요?</a>
+                        </div>
+                    </div>
+                    <button onClick={handleverifyemail} className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none">
+                        로그인
+                    </button>
+                    <button onClick={handleGoogleLogin} className="w-full px-4 py-2 mt-1 font-bold text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none">
+                        Google로 로그인하기
+                    </button>
+                    <div className="mt-4 text-center">
+                        <p className="text-sm text-gray-600">계정이 없으신가요? <a href="#" className="text-blue-500 hover:underline">회원가입</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        // <div className="flex items-center justify-center h-screen">
+        //     <div className='boxGroup'>
+        //         <div className="p-4 bg-gray-100 rounded-lg loginBox">
+        //             <div className="mb-4 inputBox">
+        //                 <input type="text" className="w-full px-4 py-2 border border-gray-300 rounded inputField focus:outline-none focus:border-blue-500" placeholder="아이디" value={email ? email : email} onChange={(e) => setEmail(e.target.value)} />
+        //                 <input type="password" className="w-full px-4 py-2 mt-2 border border-gray-300 rounded inputField focus:outline-none focus:border-blue-500" placeholder="비밀번호" onChange={(e) => setPassword(e.target.value)} />
+        //             </div>
+        //             <button className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded loginButton hover:bg-blue-600" onClick={handleverifyemail}>로그인</button>
+        //         </div>
+        //         <div className="mt-4 text-center findBox">
+        //             <button id='findId' className="text-blue-500 hover:underline">아이디/비밀번호 찾기</button>
+        //             <h5 className="mt-2">계정이 없으신가요?</h5>
+        //             <div className="flex justify-center mt-4">
+        //                 <button onClick={handleGoogleLogin} className="px-4 py-2 mr-2 font-bold text-white bg-red-500 rounded hover:bg-red-600">구글로그인</button>
+        //                 <button id='join' onClick={()=>{navigate('/join')}} className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-600">회원가입</button>
+        //             </div>
+        //         </div>
+        //     </div >
+        // </div >
     );
 }
 
