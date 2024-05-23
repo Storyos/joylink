d365673a78566e47dd0832f6ea098b9df5d350bd
seyun00 <reny83@naver.com>
2024-05-23 09:41:19 +0900

    vr 이젤 배치_csy

--- client/src/pages/VR/vr.jsx
+++ client/src/pages/VR/vr.jsx
@@ -2,33 +2,17 @@ import React, { useEffect, useRef } from "react";
 import "aframe";
 import "aframe-particle-system-component";
 import "aframe-extras"
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
 
 export default function Vr() {
   const tent = "/tent.glb"
-  const sakura ="/sakura.glb"
+  const sakura ="/sakura_tree.glb"
   
   const cherryBlossomImgPath = "/cherry_blossom.png";
   const grassImgPath ="/grass.jpg";
-  const easelFbxPath = "/easel.fbx";
   
-  const CountCherryBlossom = Array.from({length: 6}, () => 0); // 벚꽃나무 이미지 좌우 6세트 배열로 표현
-  const CountGrass = Array.from({length: 9}, () => 0); // 풀밭 이미지 9세트 배열로 표현
+  const CountCherryBlossom = Array.from({length: 10}, () => 0); // 벚꽃나무 이미지 좌우 10세트 배열로 표현
+  const CountGrass = Array.from({length: 13}, () => 0); // 풀밭 이미지 13세트 배열로 표현
   const tentline = Array.from({length: 13}, () => 0);  
-  
-  const FbxModel = ({ url, position, scale, rotation }) => {
-    const modelRef = useRef();
-  
-    useEffect(() => {
-      const loader = new FBXLoader();
-      loader.load(url, (object) => {
-        object.scale.set(scale[0], scale[1], scale[2]);
-        modelRef.current.object3D.add(object);
-      });
-    }, [url, scale]);
-  
-    return <a-entity ref={modelRef} position={position} rotation={rotation}></a-entity>;
-  };
 
   return (
     <div>
@@ -40,80 +24,84 @@ export default function Vr() {
 
         {/* 하늘 */}
         <a-sky color="#9CCEFF"></a-sky>
-
-        {/* 이젤 */}
-        <a-entity id="easel" 
-                  gltf-model="url(easel.glb)"
-                  scale = "0.2 0.2 0.2"
-                  position="-3 1 2" >
-        </a-entity>
-        
-        <FbxModel url ={easelFbxPath} 
-        position="0 1.0 0"
-        scale={[0.001, 0.001, 0.001]}
-        rotation="0 180 0"/>
-        
-        {/* 벚꽃나무 (왼쪽) */}
-        {CountCherryBlossom.map((_, index)=>(
-          <a-image src={cherryBlossomImgPath} 
-          position={`-14 5.3 ${3-(index*9)}`} 
-          width="9" 
-          height="9"
-          rotation="0 90 0">
-          </a-image>
-        ))}
-
-        {/* 벚꽃나무 (오른쪽) */}
-        {CountCherryBlossom.map((_, index)=>(
-          <a-image src={cherryBlossomImgPath} 
-          position={`14 5.3 ${3-(index*9)}`} 
-          width="9" 
-          height="9"
-          rotation="0 -90 0">
-          </a-image>
-        ))}
         
         {/* 풀밭 이미지 */}
         {CountGrass.map((_, index)=>(
           <>
             <a-image src={grassImgPath} 
-                    position={`0 1 ${4.5-(6*index)}`}
+                    position={`0 0 ${4.5-(6*index)}`}
                     width="6" 
                     height="6"
                     rotation="90 90 0">
             </a-image>
             <a-image src={grassImgPath} 
-                    position={`6 1 ${4.5-(6*index)}`}
+                    position={`6 0 ${4.5-(6*index)}`}
                     width="6" 
                     height="6"
                     rotation="90 90 0">
             </a-image>
             <a-image src={grassImgPath} 
-                    position={`-6 1 ${4.5-(6*index)}`} 
+                    position={`-6 0 ${4.5-(6*index)}`} 
+                    width="6" 
+                    height="6"
+                    rotation="90 90 0">
+            </a-image>
+            <a-image src={grassImgPath} 
+                    position={`12 0 ${4.5-(6*index)}`}
+                    width="6" 
+                    height="6"
+                    rotation="90 90 0">
+            </a-image>
+            <a-image src={grassImgPath} 
+                    position={`-12 0 ${4.5-(6*index)}`} 
                     width="6" 
                     height="6"
                     rotation="90 90 0">
             </a-image>
           </>
         ))}
+        
+        {/* 동아리 부스 */}
         {tentline.map((_, index)=>(
           <a-entity gltf-model={`url(${tent})`} 
-          position={`-6 1 ${5-(index*4)}`}></a-entity>
+          position={`-8 0 ${5-(index*6)}`}
+          scale="1.2 1.2 1.2"></a-entity>
         ))}
         {tentline.map((_, index)=>(
           <a-entity gltf-model={`url(${tent})`} 
-          position={`6 1 ${5-(index*4)}`}></a-entity>
+          position={`8 0 ${5-(index*6)}`}
+          scale="1.2 1.2 1.2"></a-entity>
         ))}
+        
+        {/* 이젤 */}
+        {tentline.map((_, index)=>(
+          <a-entity id="easel" 
+                  gltf-model="url(easel.glb)"
+                  scale = "0.3 0.3 0.3"
+                  position={`-5 0 ${3.5-(index*6)}`}
+                  rotation="0 -90 0" >
+          </a-entity>
+        ))}
+        {tentline.map((_, index)=>(
+          <a-entity id="easel" 
+                  gltf-model="url(easel.glb)"
+                  scale = "0.3 0.3 0.3"
+                  position={`5 0 ${6.5-(index*6)}`}
+                  rotation="0 90 0" >
+          </a-entity>
+        ))}
+        
+        {/* 벚꽃나무 */}
         {CountCherryBlossom.map((_, index)=>(
           <a-entity gltf-model={`url(${sakura})`} 
-          position={`-11 0.8 ${8-(index*8)}`}
-          scale="0.5 0.5 0.5"
+          position={`-12 0 ${6-(index*8)}`}
+          scale="30 30 30"
           rotation={`0 ${60*index}  0`}></a-entity>
         ))}
         {CountCherryBlossom.map((_, index)=>(
           <a-entity gltf-model={`url(${sakura})`} 
-          position={`11 0.8 ${8-(index*8)}`}
-          scale="0.5 0.5 0.5"
+          position={`12 0 ${6-(index*8)}`}
+          scale="30 30 30"
           rotation={`0 ${60*index}  0`}></a-entity>
         ))}
       </a-scene>
