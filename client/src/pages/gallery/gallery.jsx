b47d9db338ba4066cbee86ecafaccb6c7b1570e0
forgetme-not <gijae8888@naver.com>
2024-05-23 09:42:36 +0900

    pgj 동아리별 페이지 디자인 수정

--- client/src/pages/gallery/gallery.jsx
+++ client/src/pages/gallery/gallery.jsx
@@ -54,26 +54,35 @@ export default function Gallery() {
   }
 
   return (
-    <div className="flex justify-center mt-28">
-      <div>
-        <MyInfo />
-      </div>
-      <div className="w-1/2 p-6">
-        <div className="flex items-center justify-between my-4">
-          <h1 className="text-xl font-bold">갤러리</h1>
-          <input type="text" id="search" placeholder="갤러리 검색" className="px-2 py-1 border rounded" />
+    <div className="mt-28">
+      <h1 className="text-center text-6xl mt-40 mb-16" id="myClub_heading" style={{ fontFamily: '"Dancing Script", sans-serif' }}>
+        The #1 job board for<br></br> graphic design jobs
+      </h1>
+      <h3 className="text-center text-xl text-gray-500 mb-20">
+        Life's journey thrives on exploration, the ultimate way to connect with<br></br>
+        opportunities and discover new horizons globally.
+      </h3>
+
+      <div className="flex justify-center">
+        <div className="mb-16">
+          <MyInfo />
         </div>
-        <hr /><br />
-        <h2 className="text-lg font-semibold my-6">갤러리 목록</h2>
-        <div>
-          <ul className="flex flex-wrap">
-            {imgURLs.map((url, index) => (
-              <li key={index} className="w-1/2 p-5">
-                <img src={url} alt={`Image ${index}`}className="max-w-full rounded-lg" 
-                style={{ maxWidth: 'calc(100% - 0.5rem)' }}/>
-              </li>
-            ))}
-          </ul>
+        <div className="w-3/5 p-6">
+          <div className="flex items-center justify-between mt-8 mb-16">
+            <h1 className="text-2xl font-bold" style={{ fontFamily: '"Dancing Script", sans-serif' }}>Gallery</h1>
+            <input type="text" id="search" placeholder="갤러리 검색" className="px-2 py-1 border rounded mt-2" />
+          </div>
+          <hr/>
+
+          <div>
+            <ul className="flex flex-wrap">
+              {imgURLs.map((url, index) => (
+                <li key={index} className="w-1/3 p-5">
+                  <img src={url} alt={`Image ${index}`}className="max-w-full rounded-lg" />
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
