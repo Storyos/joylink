40df45254bca514938a3885375d50d7d1334fc7b
seyun00 <reny83@naver.com>
2024-05-28 10:26:28 +0900

    vr_밴드동아리 추가_csy

--- client/src/pages/VR/vr.jsx
+++ client/src/pages/VR/vr.jsx
@@ -3,6 +3,7 @@ import "aframe";
 import "aframe-particle-system-component";
 import "aframe-extras";
 import AFRAME from 'aframe';
+import ClubModels from "../../components/clubModels";
 
 // 카메라 이동 범위 제한하는 컴포넌트 등록
 AFRAME.registerComponent('boundary-constraint', {
@@ -95,28 +96,12 @@ export default function Vr() {
   const ball = "/vr_src/Football.glb"
   const arcitechture1 = "/vr_src/arcitechture1.png";
   const arcitechture2 = "/vr_src/arcitechture2.png"; 
-  
-  const applicationForm = "/vr_src/application_form.png";
-  const monopoly = "vr_src/monopoly.glb";
-  const boardgame = "vr_src/boardgame.glb";
-  const books1 = "vr_src/books1.glb";
-  const books2 = "vr_src/books2.glb";
-  const picture1 = "vr_src/picture1.glb";
-  const picture2 = "vr_src/picture2.glb";
-  const camera = "vr_src/camera.glb";
-  const box = "vr_src/box.glb";
-  
-  const boardgamePoster = "vr_src/boardgame_poster.png";
-  const readingPoster = "vr_src/reading_poster.png";
-  const picturePoster = "vr_src/picture_poster.png";
-  const bandPoster = "vr_src/band_poster.png";
-  
+
   const CountCherryBlossom = Array.from({length: 23}, () => 0);
   const CountGrassX = Array.from({length: 30}, () => 0);
   const CountGrassZ = Array.from({length: 17}, () => 0);
   const CountStone = Array.from({length: 9}, () => 0);
   const tentline = Array.from({length: 5}, () => 0);
-
 
   return (
     <div>
@@ -127,7 +112,7 @@ export default function Vr() {
         <a-camera 
         jump = "height: 0.5; duration: 400"
         boundary-constraint="minX: -10; maxX: 10; minZ: -70; maxZ: 7"
-        position="0 1.6 -25"
+        position="0 1.6 7"
         look-controls="enabled:true" 
         wasd-controls="acceleration: 20">
         </a-camera>
@@ -137,115 +122,6 @@ export default function Vr() {
         
         {/* 하늘 */}
         <a-sky color="#9CCEFF"></a-sky>
-        
-        {/* 보드게임 동아리 */}
-        <a-entity gltf-model={`url(${monopoly})`} 
-                  position={`8.8 1 3.5`}
-                  scale="0.001 0.001 0.001"
-                  rotation="0 -90 0">
-        </a-entity>
-        <a-entity gltf-model={`url(${boardgame})`} 
-                  position={`8.5 1.2 6`}
-                  scale="0.5 0.5 0.5"
-                  rotation="0 0 0">
-        </a-entity>
-        <a-image src={applicationForm}
-                    position={`8.8 1 -0.5`}
-                    width="0.84" 
-                    height="1.2"
-                    rotation="-90 -90 0"
-                    link-to="url: /cbDescription">
-        </a-image>
-
-        {/* 보드게임 포스터 */}
-        <a-image src={boardgamePoster}
-                    position={`5.05 1.05 6.5`}
-                    width="0.84" 
-                    height="1.2"
-                    rotation="-15 -90 0">
-        </a-image>
-        
-        {/* 독서 동아리 */}
-        <a-entity gltf-model={`url(${books1})`} 
-                  position={`8.8 1 -10`}
-                  scale="1 1 1"
-                  rotation="0 180 0">
-        </a-entity>
-        <a-entity gltf-model={`url(${books2})`} 
-                  position={`8.5 1 -11.5`}
-                  scale="0.1 0.1 0.1"
-                  rotation="0 0 0">
-        </a-entity>
-        <a-image src={applicationForm}
-                    position={`8.8 1 -15.5`}
-                    width="0.84" 
-                    height="1.2"
-                    rotation="-90 -90 0"
-                    link-to="url: /cbDescription">
-        </a-image>
-
-        {/* 독서 포스터 */}
-        <a-image src={readingPoster}
-                    position={`5.05 1.05 -8.5`}
-                    width="0.84" 
-                    height="1.2"
-                    rotation="-15 -90 0">
-        </a-image>
-
-        {/* 사진 동아리 */}
-        <a-entity gltf-model={`url(${picture1})`} 
-                  position={`8.55 1.25 -24.5`}
-                  scale="0.3 0.3 0.3"
-                  rotation="-15 -90 0">
-        </a-entity>
-        <a-entity gltf-model={`url(${picture2})`} 
-                  position={`8.55 1.4 -25.5`}
-                  scale="2 2 2"
-                  rotation="-30 0 0">
-        </a-entity>
-        <a-entity gltf-model={`url(${camera})`} 
-                  position={`8.5 1.1 -26.5`}
-                  scale="0.5 0.5 0.5"
-                  rotation="0 150 0">
-        </a-entity>
-        <a-entity gltf-model={`url(${box})`} 
-                  position={`9 1 -25`}
-                  scale="1.5 1.5 1.5"
-                  rotation="0 -90 0">
-        </a-entity>
-        <a-image src={applicationForm}
-                    position={`8.8 1 -30.5`}
-                    width="0.84" 
-                    height="1.2"
-                    rotation="-90 -90 0"
-                    link-to="url: /cbDescription">
-        </a-image>
-
-        {/* 사진 포스터 */}
-        <a-image src={picturePoster}
-                    position={`5.05 1.05 -23.5`}
-                    width="0.84" 
-                    height="1.2"
-                    rotation="-15 -90 0">
-        </a-image>
-
-        {/* 밴드 동아리 */}
-        <a-image src={applicationForm}
-                    position={`8.8 1 -45.5`}
-                    width="0.84" 
-                    height="1.2"
-                    rotation="-90 -90 0"
-                    link-to="url: /cbDescription">
-        </a-image>
-
-        {/* 밴드 포스터 */}
-        <a-image src={bandPoster}
-                    position={`5.05 1.05 -38.5`}
-                    width="0.84" 
-                    height="1.2"
-                    rotation="-15 -90 0">
-        </a-image>
-
         
         {/* 건물 이미지*/}
         <a-image src={arcitechture1}
@@ -378,6 +254,19 @@ export default function Vr() {
         position={`-5 0.2 3`}
         // scale="0.003 0.003 0.003"
         rotation="0 -90 0"></a-entity>
+
+        {/* 보드게임 동아리 */}
+        <ClubModels category="boardgame" index={0}/>
+        
+        {/* 독서 동아리 */}
+        <ClubModels category="reading" index={1}/>
+        
+        {/* 사진 동아리 */}
+        <ClubModels category="photography" index={2}/>
+
+        {/* 밴드 동아리 */}
+        <ClubModels category="band" index={3}/>
+        
       </a-scene>
     </div>
   );
