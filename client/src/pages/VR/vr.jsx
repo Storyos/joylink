6d08dd21d15fe9b25fdd3bfc657c57787968e15b
Park HyeonJun <baghyeonjun16@gmail.com>
2024-06-03 09:05:36 +0900

    PHJ_작업 시작 전 제출

--- client/src/pages/VR/vr.jsx
+++ client/src/pages/VR/vr.jsx
@@ -103,6 +103,8 @@ export default function Vr() {
   return (
     <div>
       <h1>vr 페이지 입니다</h1>
+      <h1>vr 페이지 입니다</h1>
+      <h1>vr 페이지 입니다</h1>
       <a-scene className="aframe-scene">
 
         {/* 카메라 */}
