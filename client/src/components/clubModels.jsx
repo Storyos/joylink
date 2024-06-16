319f9f7cef9ec583b0f5b0bbe35c94cc707a60fd
Storyos <storyoser@pukyong.ac.kr>
2024-06-16 16:27:09 +0900

    VR 존재 모델들 대폭수정 완료

--- client/src/components/clubModels.jsx
+++ client/src/components/clubModels.jsx
@@ -4,23 +4,25 @@ export default function ClubModels({ category, index }) {
 
   // 보드게임 동아리
   const monopoly = "vr_src/boardgame_club/new_monopoly.glb";
-  const boardgame = "vr_src/boardgame_club/new_boardgame.glb";
+  const checker = "vr_src/boardgame_club/checkers.glb";
+  const boardgame = "vr_src/boardgame_club/chess.glb";
   const boardgamePoster = "vr_src/boardgame_club/boardgame_poster.png";
   const boardgameBanner = "vr_src/boardgame_club/boardgame_banner.png";
   const boardgameBanner2 = "vr_src/boardgame_club/boardgame_banner2.png";
 
   // 독서 동아리
-  const books1 = "vr_src/reading_club/new_books1.glb";
-  const books2 = "vr_src/reading_club/new_books2.glb";
+  const books1 = "vr_src/reading_club/books.glb";
+  const bookopen = "vr_src/reading_club/bookopen.glb";
+  const books2 = "vr_src/reading_club/tea_time.glb";
   const readingPoster = "vr_src/reading_club/reading_poster.png";
   const readingBanner = "vr_src/reading_club/reading_banner.png";
   const readingBanner2 = "vr_src/reading_club/reading_banner2.png";
 
   // 사진 동아리
-  const picture1 = "vr_src/photography_club/new_picture1.glb";
+  const picture1 = "vr_src/photography_club/wall_decoration_picture.glb";
   const picture2 = "vr_src/photography_club/new_picture2.glb";
-  const camera = "vr_src/photography_club/new_camera.glb";
-  const box = "vr_src/photography_club/new_box.glb";
+  const camera = "vr_src/photography_club/reflex_camera.glb";
+  const box = "vr_src/photography_club/cardboard_box.glb";
   const photographyPoster = "vr_src/photography_club/photography_poster.png";
   const photographyBanner = "vr_src/photography_club/photography_banner.png";
   const photographyBanner2 = "vr_src/photography_club/photography_banner2.png";
@@ -33,7 +35,6 @@ export default function ClubModels({ category, index }) {
   const bandBanner = "vr_src/band_club/band_banner.jpg";
   const bandBanner2 = "vr_src/band_club/band_banner2.png";
   const speaker = "vr_src/band_club/new_audio_speaker.glb";
-  // const mixer = "vr_src/band_club/new_audio_mixer.glb";
 
   // 등산 동아리
   const backpack = "vr_src/climbing_club/new_backpack.glb";
@@ -56,29 +57,30 @@ export default function ClubModels({ category, index }) {
 
   // 축구동아리 소스
   // const footballshoes = "vr_src/football_club/football_shoes.glb"
-  const football_set = "vr_src/football_club/football_set.glb"
+  const football_set = "vr_src/football_club/new_ball.glb"
+  const football_goal = "vr_src/football_club/football_goal.glb"
   const soccer_uniform = "vr_src/football_club/soccer_uniform.glb"
   const cone = "vr_src/football_club/cone.glb"
   const trophy = "/vr_src/football_club/trophy.glb"
   const footballposter = "/vr_src/football_club/footballclub.png";
-  const ball = "/vr_src/football_club/football.glb"
-  const footballtable = "/vr_src/football_club/football_Gambling.glb"
+  const ball = "/vr_src/football_club/new_ball.glb"
+  const footballtable = "/vr_src/football_club/new_football_Gambling.glb"
   const footballbenner = "vr_src/football_club/footballbenner.png"
   const footballbenner2 = "vr_src/football_club/footballbenner2.png"
   //로봇제어계측 동아리
   const mechanic_arm = "vr_src/mechanic_club/mechanic_arm.glb"
-  const drone = "vr_src/mechanic_club/new_drone.glb"
-  const drone2 = "vr_src/mechanic_club/new_drone2.glb"
+  const workshop = "vr_src/mechanic_club/machine_workshop.glb"
+  const drone2 = "vr_src/mechanic_club/drone_low_poly.glb"
   const tool_box = "vr_src/mechanic_club/tool_box.glb"
-  const controller = "vr_src/mechanic_club/controller.glb"
+  const controller = "vr_src/mechanic_club/new_controller.glb"
   const highrowposter = "vr_src/mechanic_club/highrow.jpg"
   const highrowbanner = "vr_src/mechanic_club/highrowbanner.png"
   const highrowbanner2 = "vr_src/mechanic_club/highrowbanner2.png"
   //술 동아리
   const alcohol_set = "vr_src/alcohol/new_bottle_set_alcohol.glb"
   const wine = "vr_src/alcohol/new_wine.glb"
-  const oak = "vr_src/alcohol/new_oak.glb"
-  const certificate = "vr_src/alcohol/new_certificate.glb"
+  const mini_alcohol_bar = "vr_src/alcohol/newbottle_set_alcohol.glb"
+  const barrel = "vr_src/alcohol/barrel.glb"
   const alcoholposter = "vr_src/alcohol/alcoholposter.jpg"
   const alcoholbanner = "vr_src/alcohol/alcoholbanner.png"
   const alcoholbanner2 = "vr_src/alcohol/alcoholbanner2.png"
@@ -95,20 +97,21 @@ export default function ClubModels({ category, index }) {
   const newspaper = "vr_src/stockclub/newspaper.glb"
   //사람 오브젝트
   // const soccerman = "vr_src/people/newsoccerman.glb"
-  const soccerman2 = "vr_src/people/soccergirl.glb"
+  const soccergirl = "vr_src/people/soccergirl.glb"
   const korean = "vr_src/people/newkorean.glb"
   const korean2 = "vr_src/people/newkorean2.glb"
   // const korean3 = "vr_src/people/newkorean3.glb"
   const korean4 = "vr_src/people/korean4.glb"
-  const korean5 = "vr_src/people/newkorean5.glb"
+  const korean5 = "vr_src/people/franklin.glb"
   const person1 = "vr_src/people/person1.glb";
   const person2 = "vr_src/people/person2.glb";
   const person3 = "vr_src/people/person3.glb";
-  const person4 = "vr_src/people/koreangirl2.glb";
-  const person5 = "vr_src/people/newperson5.glb";
-  const person6 = "vr_src/people/gman.glb";
-  const person7 = "vr_src/people/koreangirl.glb";
-  const person8 = "vr_src/people/new_athletic_man.glb";
+  const person4 = "vr_src/people/person4.glb";
+  // newperson5 교체 필요.
+  const person5 = "vr_src/people/newperson55.glb";
+  const person6 = "vr_src/people/person6.glb";
+  const person7 = "vr_src/people/person7.glb";
+  const person8 = "vr_src/people/person8.glb";
 
   return (
     <>
@@ -123,8 +126,14 @@ export default function ClubModels({ category, index }) {
             distance-culling="maxDistance: 20">
           </a-entity>
           <a-entity gltf-model={`url(${boardgame})`}
-            position={`8.5 1.2 ${6 - (15 * index)}`}
-            scale="0.5 0.5 0.5"
+            position={`8.5 1 ${6 - (15 * index)}`}
+            scale="0.08 0.08 0.08"
+            rotation="0 90 0"
+            distance-culling="maxDistance: 20">
+          </a-entity>
+          <a-entity gltf-model={`url(${checker})`}
+            position={`8.5 1 ${5.5 - (15 * index)}`}
+            scale="1.5 1.5 1.5"
             rotation="0 0 0"
             distance-culling="maxDistance: 20">
           </a-entity>
@@ -164,14 +173,20 @@ export default function ClubModels({ category, index }) {
       {category === "reading" &&
         <>
           <a-entity gltf-model={`url(${books1})`}
-            position={`8.8 1 ${5 - (15 * index)}`}
-            scale="1 1 1"
+            position={`9.2 1 ${5 - (15 * index)}`}
+            scale="0.0018 0.0018 0.0018"
+            rotation="0 180 0"
+            distance-culling="maxDistance: 20">
+          </a-entity>
+          <a-entity gltf-model={`url(${bookopen})`}
+            position={`8.3 1 ${5 - (15 * index)}`}
+            scale="1.2 1.2 1.2"
             rotation="0 180 0"
             distance-culling="maxDistance: 20">
           </a-entity>
           <a-entity gltf-model={`url(${books2})`}
             position={`8.5 1 ${3.5 - (15 * index)}`}
-            scale="0.1 0.1 0.1"
+            scale="0.5 0.5 0.5"
             rotation="0 0 0"
             distance-culling="maxDistance: 20">
           </a-entity>
@@ -211,27 +226,27 @@ export default function ClubModels({ category, index }) {
       {category === "photography" &&
         <>
           <a-entity gltf-model={`url(${picture1})`}
-            position={`8.55 1.25 ${5.5 - (15 * index)}`}
-            scale="0.3 0.3 0.3"
-            rotation="-15 -90 0"
+            position={`8.15 1.0 ${6.1 - (15 * index)}`}
+            scale="1 1 1"
+            rotation="0 180 90"
             distance-culling="maxDistance: 20">
           </a-entity>
           <a-entity gltf-model={`url(${picture2})`}
-            position={`8.55 1.4 ${4.5 - (15 * index)}`}
+            position={`8.55 1.4 ${4.1 - (15 * index)}`}
             scale="2 2 2"
             rotation="-30 0 0"
             distance-culling="maxDistance: 20">
           </a-entity>
           <a-entity gltf-model={`url(${camera})`}
-            position={`8.5 1.1 ${3.5 - (15 * index)}`}
-            scale="0.5 0.5 0.5"
-            rotation="0 150 0"
+            position={`8.5 1.2 ${3 - (15 * index)}`}
+            scale="0.03 0.03 0.03"
+            rotation="0 270 0"
             distance-culling="maxDistance: 20">
           </a-entity>
           <a-entity gltf-model={`url(${box})`}
-            position={`9 1 ${5 - (15 * index)}`}
-            scale="1.5 1.5 1.5"
-            rotation="0 -90 0"
+            position={`8.93 1.2 ${4.8 - (15 * index)}`}
+            scale="1 1 1"
+            rotation="0 90 0"
             distance-culling="maxDistance: 20">
           </a-entity>
           <a-image src={applicationForm}
@@ -419,13 +434,13 @@ export default function ClubModels({ category, index }) {
             distance-culling="maxDistance: 20"
           >
           </a-entity>
-          <a-entity gltf-model={`url(${steak1})`}
+          {/* <a-entity gltf-model={`url(${steak1})`}
             position={`-8.3 1 ${-1.5 - (15 * index)}`}
             scale="1.5 1.5 1.5"
             rotation="0 0 0"
             distance-culling="maxDistance: 20"
           >
-          </a-entity>
+          </a-entity> */}
           <a-entity gltf-model={`url(${steak2})`}
             position={`-8.3 1 ${-1 - (15 * index)}`}
             scale="0.05 0.05 0.05"
@@ -506,15 +521,15 @@ export default function ClubModels({ category, index }) {
           </a-image>
           {/* 축구게임테이블 */}
           <a-entity gltf-model={`url(${footballtable})`}
-            position={`-5 0.6 ${0 - (15 * index)}`}
-            scale="0.3 0.3 0.3"
-            rotation="0 -90 0"
+            position={`-5 0.2 ${0 - (15 * index)}`}
+            scale="0.25 0.25 0.25"
+            rotation="0 -0 0"
             distance-culling="maxDistance: 20"
           ></a-entity>
           {/* 축구공 */}
           <a-entity gltf-model={`url(${ball})`}
             position={`-5 0.2 ${2 - (15 * index)}`}
-            scale="1.5 1.5 1.5"
+            scale="0.005 0.005 0.005"
             rotation="0 -90 0"
             distance-culling="maxDistance: 20"
           ></a-entity>
@@ -535,11 +550,20 @@ export default function ClubModels({ category, index }) {
         position={`-6 0.5 ${1-(15*index)}`}
         scale="1.5 1.5 1.5"
         rotation="0 -90 0"></a-entity> */}
+
           <a-entity gltf-model={`url(${football_set})`}
-            position={`-6 0.5 ${-4 - (15 * index)}`}
+            position={`-6 0.25 ${-4 - (15 * index)}`}
             rotation="0 90 0"
+            scale="0.005 0.005 0.005"
             distance-culling="maxDistance: 20"
           ></a-entity>
+          <a-entity gltf-model={`url(${football_goal})`}
+            position={`-6 1.3 ${-4 - (15 * index)}`}
+            rotation="0 90 0"
+            scale="0.01 0.01 0.01"
+            distance-culling="maxDistance: 20"
+          ></a-entity>
+
           <a-entity gltf-model={`url(${soccer_uniform})`}
             position={`-9 1.2 ${-2 - (15 * index)}`}
             rotation="0 180 0"
@@ -616,16 +640,16 @@ export default function ClubModels({ category, index }) {
             distance-culling="maxDistance: 20"
           ></a-entity>
           {/*드론*/}
-          <a-entity gltf-model={`url(${drone})`}
+          <a-entity gltf-model={`url(${workshop})`}
             position={`-8.5 1 ${-0.5 - (15 * index)}`}
-            scale="0.4 0.4 0.4"
-            rotation="0 -90 0"
+            scale="0.2 0.2 0.2"
+            rotation="0 -0 0"
             distance-culling="maxDistance: 20"
           ></a-entity>
           {/*드론2*/}
           <a-entity gltf-model={`url(${drone2})`}
-            position={`-8.6 1.2 ${0 - (15 * index)}`}
-            scale="0.025 0.025 0.025"
+            position={`-8.6 1.2 ${1 - (15 * index)}`}
+            scale="0.005 0.005 0.005"
             rotation="0 -90 0"
             distance-culling="maxDistance: 20"
           ></a-entity>
@@ -636,7 +660,9 @@ export default function ClubModels({ category, index }) {
             distance-culling="maxDistance: 20"
           ></a-entity>
           <a-entity gltf-model={`url(${controller})`}
-            position={`-8 1 -13`}
+            scale= "0.08 0.08 0.08"
+            rotation= "0 65 0"
+            position={`-8.3 1.15 -13.8`}
             distance-culling="maxDistance: 20"
           ></a-entity>
           <a-image src={applicationForm}
@@ -705,17 +731,17 @@ export default function ClubModels({ category, index }) {
             distance-culling="maxDistance: 20"
           ></a-entity>
           {/* 오크통 */}
-          <a-entity gltf-model={`url(${oak})`}
-            position={`-8.1 1.6 ${-2 - (15 * index)}`}
-            scale="0.4 0.4 0.4"
+          <a-entity gltf-model={`url(${mini_alcohol_bar})`}
+            position={`-8.5 1.05 ${-1 - (15 * index)}`}
+            scale="0.6 0.6 0.6"
             rotation="0 90 0"
             distance-culling="maxDistance: 20"
           ></a-entity>
           {/* 자격증 */}
-          <a-entity gltf-model={`url(${certificate})`}
-            position={`-9 1 ${-0.5 - (15 * index)}`}
-            scale="0.0015 0.0015 0.0015"
-            rotation="-20 90 0"
+          <a-entity gltf-model={`url(${barrel})`}
+            position={`-9 0.9 ${0 - (15 * index)}`}
+            scale="0.3 0.3 0.3"
+            rotation="-0 90 0"
             distance-culling="maxDistance: 20"
           ></a-entity>
           <a-image src={applicationForm}
@@ -809,7 +835,7 @@ export default function ClubModels({ category, index }) {
             rotation="0 90 0"
             distance-culling="maxDistance: 20"
           ></a-entity> */}
-          <a-entity gltf-model={`url(${soccerman2})`}
+          <a-entity gltf-model={`url(${soccergirl})`}
             position={`-4.4 0 0 `}
             rotation="0 -90 0"
             distance-culling="maxDistance: 20"
@@ -839,22 +865,22 @@ export default function ClubModels({ category, index }) {
             distance-culling="maxDistance: 20"
           ></a-entity>
           <a-entity gltf-model={`url(${korean5})`}
-            position={`-6 0.05 -29.5 `}
-            rotation="0 -60 0"
+            position={`-5.9 0.05 -29.7 `}
+            rotation="0 90 0"
             scale="0.7 0.7 0.7"
             distance-culling="maxDistance: 20"
           ></a-entity>
           <a-entity gltf-model={person1}
             position="-6.5 0.1 -61"
-            scale="0.025 0.025 0.025"
+            scale="0.02 0.02 0.02"
             rotation="0 -135 0"
             distance-culling="maxDistance: 20"
           >
           </a-entity>
           <a-entity gltf-model={person2}
-            position="5 0.1 -10"
-            scale="0.01 0.01 0.01"
-            rotation="0 0 0"
+            position="4 0.1 -8.5"
+            scale="0.018 0.018 0.018"
+            rotation="0 85 0"
             distance-culling="maxDistance: 20"
           >
           </a-entity>
@@ -873,15 +899,15 @@ export default function ClubModels({ category, index }) {
           >
           </a-entity>
           <a-entity gltf-model={person5}
-            position="-6 1 -45"
-            scale="1 1 1"
+            position="-6 0.2 -45"
+            scale="0.018 0.018 0.018"
             rotation="0 -90 0"
             distance-culling="maxDistance: 20"
           >
           </a-entity>
           <a-entity gltf-model={person6}
             position="5 0.1a 0"
-            scale="0.03 0.027 0.02"
+            scale="0.02 0.02 0.02"
             rotation="-0 90 0"
             distance-culling="maxDistance: 20"
           >
